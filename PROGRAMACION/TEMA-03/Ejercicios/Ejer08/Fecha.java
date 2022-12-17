3be4affdd27f1c63634ffff05a46370265192b12
Diego Simbaña <d.simbana85@outlook.com>
2022-12-17 23:40:04 +0100

    Se me hace dificil ver el binario

---
+++ PROGRAMACION/TEMA-03/Ejercicios/Ejer08/Fecha.java
@@ -0,0 +1,210 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioa0408;
+
+/**
+ *
+ * @author Anuska
+ */
+public class Fecha {
+    
+    // Atributos
+    private int d;
+    private int m;
+    private int a;
+    
+    
+    // Contructores
+    
+    public Fecha()
+    {      
+    }
+    public Fecha(int d, int m, int a)
+    {
+        if (fechaCorrecta(d,m,a))
+        {
+            this.d = d;
+            this.m = m;
+            this.a = a;
+        }
+    }
+    
+    public static boolean esBisiesto(int a)
+    {
+        return (a%4==0 && !(a%100==0 && a%400!=0));
+    }
+    
+    public static boolean fechaCorrecta(int d, int m, int a)
+    {
+        if (m<1 || m>12)
+            return false;
+        else
+        {
+            switch (m)
+            {
+                case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                    if (d<1 || d>31)
+                        return false;
+                    break;
+                case 4: case 6: case 9: case 11:
+                    if (d<1 || d>30)
+                        return false;
+                    break;
+                default:
+                    if (esBisiesto(a) && (d<1 || d>29))
+                        return false;
+                    else if (!esBisiesto(a) && (d<1 || d>28))
+                            return false;
+                 
+            }
+            return true;
+        }                
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public void setD(int d) {
+        if (fechaCorrecta(d,this.m,this.a))
+            this.d = d;
+    }
+
+    public int getM() {
+        return m;
+    }
+
+    public void setM(int m) {
+        if (fechaCorrecta(this.d,m,this.a))
+          this.m = m;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        if (fechaCorrecta(this.d,this.m,a))
+            this.a = a;
+    }
+    
+    public void diaSiguiente()
+    {
+        if (fechaCorrecta(d, m, a))
+        {
+            if (m==1 || m==3 || m==5 || m==7 || m==8 || m==10 || m==12)
+            {
+                if (d==31 && m!=12)
+                {
+                    d=1;
+                    m++;
+                }
+                else if (d==31 && m==12)
+                {
+                    a++;
+                    m = 1;
+                    d = 1;
+                }
+            
+                else
+                {
+                    d++;
+                }
+            }
+            else if (m==4 || m==6 || m==9 || m==11)
+            {
+                if (d==30)
+                {
+                    d=1;
+                    m++;
+                }
+                else
+                {
+                    d++;
+                }
+            }
+            else if (m==2)
+            {
+                if (esBisiesto(a))
+                {
+                    if (d==29)
+                    {
+                        m++;
+                        d=1;
+                    }
+                    else
+                    {
+                        d++;
+                    }
+                }
+                else
+                {
+                    if (d==28)
+                    {
+                        m++;
+                        d=1;
+                    }
+                    else
+                    {
+                        d++;
+                    }
+                }
+            }
+        }
+        
+    }
+    
+    public void diaSiguiente2()
+    {
+        int ultDia;
+        switch(m)
+        {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                ultDia = 31;
+                break;
+            case 4: case 6: case 9: case 11:    
+                ultDia = 30;
+                break;
+            default:
+                if (esBisiesto(a))
+                    ultDia = 29;
+                else
+                    ultDia = 28;  
+        }
+        
+        if (d==ultDia && m!=12)
+        {
+            d=1;
+            m++;
+        }
+        else if (d==ultDia && m==12)
+        {
+            a++;
+            m = 1;
+            d = 1;
+        }
+        else
+        {
+            d++;
+        }
+    }
+    
+    @Override
+    public String toString()
+    {
+        String dia,mes;
+        if (d<10)
+            dia = "0"+d;
+        else
+            dia = ""+d;
+        if (m<10)
+            mes = "0"+m;
+        else
+            mes = ""+m;
+        return dia+"-"+mes+"-"+a;
+    }
+    
+}
---
+++ PROGRAMACION/TEMA-03/Ejercicios/Ejer10/Binario.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioa0410;
+
+/**
+ *
+ * @author Anuska
+ */
+public class Binario {
+
+    private long valor;
+
+    public Binario() {
+
+    }
+
+    public Binario(long n) {
+        valor = n;
+    }
+
+    public Binario(int n) {
+        valor = toBinario(n);
+    }
+
+    public long toBinario(int n) {
+        if (n < 2) {
+            return n;
+        } else {
+            return toBinario(n / 2) * 10 + (n % 2);
+        }
+    }
+
+    public long getValor() {
+        return valor;
+    }
+
+    public void setValor(long valor) {
+        this.valor = valor;
+
+    }
+
+    public int toDecimal() {
+        long n = valor;
+        int nDec = 0;
+        int pos = 0;
+        while (n > 0) {
+            nDec = nDec + (int) (n % 10) * (int) Math.pow(2, pos);
+            pos++;
+            n = n / 10;
+        }
+        return nDec;
+    }
+
+    public static boolean esValido(long n) {
+        if (n == 0 || n == 1) {
+            return true;
+        } else {
+            int ultima = (int) n % 10;
+
+            return (ultima == 0 || ultima == 1) && esValido(n / 10);
+
+        }
+    }
+
+    public boolean esValido() {
+        return esValido(valor);
+    }
+
+}
