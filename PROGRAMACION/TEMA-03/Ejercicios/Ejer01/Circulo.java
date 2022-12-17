4eeea036e614b8919ec92d1408490e060ab26331
Diego Simbaña <d.simbana85@outlook.com>
2022-12-17 11:16:18 +0100

    Se añaden mas documentos de entornos de desarrollo.

---
+++ PROGRAMACION/TEMA-03/Ejercicios/Ejer01/Circulo.java
@@ -0,0 +1,41 @@
+
+package ejercicioa0401;
+
+public class Circulo {
+    
+    // Atributos
+    double radio;
+    final double PI = 3.14159;
+    
+    // Constructores
+    public Circulo()
+    {
+        radio = 1;
+    }
+    
+    public Circulo(double r)
+    {
+        radio = r;
+    }
+    
+    public double getRadio()
+    {
+        return radio;
+    }
+    
+    public void setRadio(double r)
+    {
+        radio = r;
+    }
+    
+    public double perimetro()
+    {
+        return 2*PI*radio;
+    }
+    
+    public double area()
+    {
+        return PI*radio*radio;
+    }
+    
+}
---
+++ PROGRAMACION/TEMA-03/Ejercicios/Ejer01/EjercicioA0401.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioa0401;
+import java.util.Scanner;
+
+public class EjercicioA0401 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Escribe el valor del radio");
+        double radio = sc.nextDouble();
+        Circulo c1 = new Circulo(radio);
+        Circulo c2 = new Circulo();
+        
+        System.out.println("Escribe el valor del radio para el segundo círculo");
+        radio = sc.nextDouble();
+        c2.setRadio(radio);
+        
+        System.out.println("El radio del primer círculo es "+c1.getRadio());
+        System.out.println("El perímetro del primer círculo es "+c1.perimetro());
+        System.out.println("El area del primer círculo es "+c1.area());
+        System.out.println("El radio del segundo círculo es "+c2.getRadio());
+        System.out.println("El perímetro del segundo círculo es "+c2.perimetro());
+        System.out.println("El area del segundo círculo es "+c2.area());
+        
+    }
+    
+}
