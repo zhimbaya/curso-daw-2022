b65f2bde4042134275287e3d121ede58d4b593d4
Diego Simbaña <diegoarmandosim@outlook.com>
2023-03-18 12:06:47 +0100

    se actualiza.

---
+++ PROGRAMACION/TEMA-08/EJERCICIOS/Numero0a100.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tema08;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author diego
+ */
+public class Numero0a100 {
+
+    public static void main(String[] args) {
+        
+        int numero = -1;
+        String linea;
+        int intentos = 0;
+        BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+
+        do {
+            try {
+                System.out.println("Introducir un número de 0 al 100");
+                linea = teclado.readLine();
+                numero = Integer.parseInt(linea);
+            } catch (IOException e) {
+                System.out.println("Error al leer por el teclado.");
+            } catch (NumberFormatException e) {
+                System.out.println("Debe introducir un número del 0 al 100.");
+            } finally {
+                intentos++;
+            }
+
+        } while (numero < 0 || numero > 100);
+        
+        System.out.println("El número introducido es: " + numero);
+        System.out.println("Número de intentos: " + intentos);
+    }
+
+}
