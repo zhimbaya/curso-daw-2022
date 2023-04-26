de76e75e23726542b651e7b47db5e5b74a0a4593
Diego Simbaña <diegoarmandosim@outlook.com>
2023-04-26 06:51:54 +0200

    Se sigue con los ejercicios propuestos.

--- PROGRAMACION/TEMA-10/EJERCICIOS/ActividadUT10-4-LeerFicheroCaracterACaracter/LeerFicheroTexto.java
+++ PROGRAMACION/TEMA-10/EJERCICIOS/ActividadUT10-4-LeerFicheroCaracterACaracter/LeerFicheroTexto.java
@@ -1,85 +1,70 @@
+package Tema10;
+
 /*4)Escribe un programa denominado LeerFicheroTexto  que se ejecute de la siguiente forma: 
  C> java LeerFicheroTexto  texto.txt.
-El  programa deberá controlar que el número de parámetros pasados a main es correcto, 
-manejará excepciones de tipo IOException, 
-si el número de argumentos es correcto, 
-	invocará a un método denominado mostrarFichero, 
-	donde se llevará a cabo la visualización del fichero de texto. 
-Si el número de parámetros no es correcto 
-	lanzará una excepción propia indicando que los parámetros son incorrectos 
+El  programa deberï¿½ controlar que el nï¿½mero de parï¿½metros pasados a main es correcto, 
+manejarï¿½ excepciones de tipo IOException, 
+si el nï¿½mero de argumentos es correcto, 
+	invocarï¿½ a un mï¿½todo denominado mostrarFichero, 
+	donde se llevarï¿½ a cabo la visualizaciï¿½n del fichero de texto. 
+Si el nï¿½mero de parï¿½metros no es correcto 
+	lanzarï¿½ una excepciï¿½n propia indicando que los parï¿½metros son incorrectos 
 	y la sintaxis correcta para ejecutar el programa. 
-La información se procesará carácter a carácter.*/
+La informaciï¿½n se procesarï¿½ carï¿½cter a carï¿½cter.*/
 
 import java.io.*;
 import java.lang.*;
 
-public class LeerFicheroTexto
-{
-	public static void main(String args[]) throws IOException
-	{
-		try
-		{
-			if(args.length != 1)
-				throw new ArgumentosIncorrectos();
-				
-				else
-				{
-						mostrarFichero(args[0]);
-				}
-		}
-		
-		////////////////////////////////////////////////////////////////////////
-		
-		catch(ArgumentosIncorrectos error)
-		{
-			System.out.println("El Numero de Argumentos es Incorrecto");
-			System.out.println("Sintaxis: java CrearFicheroTexto nomFichero");
-		}
-		
-	}
-	
-	////////////////////////////////////////////////////////////////////////////
-	
-	public static void mostrarFichero(String nomFichero) throws IOException
-	{
-		File fichero = new File(nomFichero);
-		FileReader flujoEntrada = null;
-		
-		try
-		{
-			if(fichero.exists())
-			{
-				flujoEntrada = new FileReader(fichero);
-				char caracter;
-        		char eof =(char) -1;
-        		while ((caracter = (char)flujoEntrada.read()) != eof)
-            		System.out.print(caracter);
-			}
-			else
-				throw new FicheroInexistente();
-		}
-		
-		catch(FicheroInexistente error)
-		{
-			System.out.println("El Fichero No Existe");
-		}
-	}
+public class LeerFicheroTexto {
+
+    public static void main(String args[]) throws IOException {
+        try {
+            if (args.length != 1) {
+                throw new ArgumentosIncorrectos();
+            } else {
+                mostrarFichero(args[0]);
+            }
+        } ////////////////////////////////////////////////////////////////////////
+        catch (ArgumentosIncorrectos error) {
+            System.out.println("El Numero de Argumentos es Incorrecto");
+            System.out.println("Sintaxis: java CrearFicheroTexto nomFichero");
+        }
+
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    public static void mostrarFichero(String nomFichero) throws IOException {
+        File fichero = new File(nomFichero);
+        FileReader flujoEntrada = null;
+
+        try {
+            if (fichero.exists()) {
+                flujoEntrada = new FileReader(fichero);
+                char caracter;
+                char eof = (char) -1;
+                while ((caracter = (char) flujoEntrada.read()) != eof) {
+                    System.out.print(caracter);
+                }
+            } else {
+                throw new FicheroInexistente();
+            }
+        } catch (FicheroInexistente error) {
+            System.out.println("El Fichero No Existe");
+        }
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+class ArgumentosIncorrectos extends Exception {
 
-class ArgumentosIncorrectos extends Exception
-{
-	public ArgumentosIncorrectos()
-	{
-		super();
-	}
+    public ArgumentosIncorrectos() {
+        super();
+    }
 }
 
-class FicheroInexistente extends Exception
-{
-	public FicheroInexistente()
-	{
-		super();
-	}
+class FicheroInexistente extends Exception {
+
+    public FicheroInexistente() {
+        super();
+    }
 }
